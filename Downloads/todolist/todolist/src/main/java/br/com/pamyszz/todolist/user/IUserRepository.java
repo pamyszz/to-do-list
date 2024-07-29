f7b88c5030066a291eadcea8dbf934de0edc4d38
Pamela de Oliveira <153380356+pamyszz@users.noreply.github.com>
2024-07-28 22:39:26 -0300

    Delete Downloads/todolist/todolist directory

--- Downloads/todolist/todolist/src/main/java/br/com/pamyszz/todolist/user/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pamyszz.todolist.user;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<UserModel, UUID>{
-    UserModel findByUsername (String username);
-}
